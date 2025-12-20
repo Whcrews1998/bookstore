2f411360c1e3ca912b1290e8c1572bf1a7ed7cbc
Harris Crews <whcrews1998@gmail.com>
2025-12-20 15:37:09 -0500

    Created register endpoint in UserController.

--- backend/src/main/java/com/whcrews/bookstore/controller/UserController.java
+++ backend/src/main/java/com/whcrews/bookstore/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.whcrews.bookstore.controller;
 
+import com.whcrews.bookstore.model.Users;
 import com.whcrews.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +27,15 @@ public class UserController {
     public String whoami() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return auth.getName();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Users user) {
+       if (userService.doesUsernameExist(user.getUsername())) {
+           return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists!");
+       }
+
+       return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(user));
     }
 
     @GetMapping("/verify")
