f300059cec688a530561f2ee05b08f813c8c00f6
Whcrews1998 <whcrews1998@gmail.com>
2025-12-28 13:42:56 -0600

    Implemented 'return' book endpoint.

--- backend/src/main/java/com/whcrews/bookstore/controller/BookController.java
+++ backend/src/main/java/com/whcrews/bookstore/controller/BookController.java
@@ -9,14 +9,12 @@ import java.util.List;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -48,7 +46,16 @@ public class BookController {
 
 	@PostMapping("/return")
 	public ResponseEntity<?> returnBook(@RequestBody Book book) {
-		return null;
+		book = bookService.find(book.getAuthor(), book.getTitle());
+
+		if (book == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body("User does not have book assigned to them.");
+		}
+
+		userBookService.returnUserBook(book);
+
+		return ResponseEntity.ok("Book has been returned.");
 	}
 
 	@PostMapping("/checkout")
@@ -63,7 +70,6 @@ public class BookController {
 		UserBook userBook = new UserBook(user, book);
 
 		user.getUserBookSet().add(userBook);
-		book.getUserBookSet().add(userBook);
 
 		userBookService.saveUserBook(userBook);
 		return ResponseEntity.ok("Book successfully checked out.");
