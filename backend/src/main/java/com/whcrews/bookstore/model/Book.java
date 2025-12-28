2cec909789c12db079a2e9ea10bca9055d80ecbd
Whcrews1998 <whcrews1998@gmail.com>
2025-12-27 19:33:06 -0600

    Added JPA mapping in Book and User models.

--- backend/src/main/java/com/whcrews/bookstore/model/Book.java
+++ backend/src/main/java/com/whcrews/bookstore/model/Book.java
@@ -1,51 +1,57 @@
 package com.whcrews.bookstore.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Book {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  private String title;
-  private String author;
-  private String coverImageName;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String author;
+	private String coverImageName;
 
-  public Long getId() {
-    return id;
-  }
+	@OneToMany(mappedBy = "book")
+	public Set<UserBook> userBookSet = new HashSet<>();
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public String getTitle() {
-    return title;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public String getTitle() {
+		return title;
+	}
 
-  public String getAuthor() {
-    return author;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public void setAuthor(String author) {
-    this.author = author;
-  }
+	public String getAuthor() {
+		return author;
+	}
 
-  public String getCoverImageName() {
-    return coverImageName;
-  }
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-  public void setCoverImageName(String coverImageName) {
-    this.coverImageName = coverImageName;
-  }
+	public String getCoverImageName() {
+		return coverImageName;
+	}
 
+	public void setCoverImageName(String coverImageName) {
+		this.coverImageName = coverImageName;
+	}
 
 }
--- backend/src/main/java/com/whcrews/bookstore/model/Users.java
+++ backend/src/main/java/com/whcrews/bookstore/model/Users.java
@@ -1,53 +1,60 @@
 package com.whcrews.bookstore.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Users implements UserDetails {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String username;
-    private String password;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String password;
 
-    public Long getId() {
-        return id;
-    }
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Set<UserBook> userBookSet = new HashSet<>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority("USER"));
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
 
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
 }
