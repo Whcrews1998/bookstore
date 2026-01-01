6e874a43182d97cacea64c149accfc82cdf5fc9e
Whcrews1998 <whcrews1998@gmail.com>
2026-01-01 09:16:43 -0600

    Added logout config to spring scecurity.

--- backend/src/main/java/com/whcrews/bookstore/configuration/SecurityConfig.java
+++ backend/src/main/java/com/whcrews/bookstore/configuration/SecurityConfig.java
@@ -4,18 +4,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -41,10 +36,12 @@ public class SecurityConfig {
                             res.setStatus(HttpServletResponse.SC_OK);
                         }).failureHandler((req, res, ex) -> {
                             res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
-                        }). permitAll()
-                ).exceptionHandling(ex -> ex.authenticationEntryPoint((req, res, authEx) -> {
+                        }).permitAll())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint((req, res, authEx) -> {
                     res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                 }))
+                .logout(logout -> logout.logoutUrl("/logout").invalidateHttpSession(true).clearAuthentication(true)
+                        .deleteCookies("JSESSIONID").logoutSuccessHandler((req, res, auth) -> res.setStatus(200)))
                 .build();
     }
 
